09985d33b1a1373cae1f7313a1a7bba273a3c2cc
ZK <823864100@qq.com>
2022-10-14 14:41:37 +0800

    four

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,14 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A==null){
+        return B ;}
+        IntList preA=A;
+        while(preA.rest!=null){
+            preA=preA.rest;
+        }
+        preA.rest=B;
+        return A;
     }
 
     /**
@@ -91,7 +98,19 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+
+            if (A == null) return B;
+            IntList ptrA = A;
+            IntList res = new IntList(0, null);
+            IntList ptrRes = res;
+            while (ptrA != null) {
+                ptrRes.rest = new IntList(ptrA.first, null);
+                ptrRes = ptrRes.rest;
+                ptrA = ptrA.rest;
+            }
+            ptrRes.rest = B;
+            return res.rest;
+
     }
 
 
